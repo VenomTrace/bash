358d824324c43c8035ac310514ed2dd4eebcfdfc
msfvenom <73685390+VenomTrace@users.noreply.github.com>
2025-05-06 08:55:13 +0530

    Create bash_port_scanner.sh

---
+++ bash_port_scanner.sh
@@ -0,0 +1,8 @@
+##Source
+#inspired by https://catonmat.net/tcp-port-scanner-in-bash
+
+#!/bin/bash
+for port in {1..1000}; do
+ timeout 1 bash -c "echo >/dev/tcp/$1/$port" 2>/dev/null && echo "port $port is open"
+done
+
